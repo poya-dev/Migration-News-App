d5152b63759648a7187b80639f14719bd9420574
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-03 22:52:51 -0800

    [ADD] Added some dummy data.

---
+++ lib/src/utils/data.dart
@@ -0,0 +1,133 @@
+List categories = [
+  {"name": "All"},
+  {"name": "Sport"},
+  {"name": "Health"},
+  {"name": "Culture"},
+  {"name": "Technology"},
+  {"name": "People"},
+  {"name": "Food"},
+];
+
+List news = [
+  {
+    "id": 102,
+    "title": "Mix Sald",
+    "image":
+        "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "320",
+    "category": "Salad",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 100,
+    "title": "Rice Pot",
+    "image":
+        "https://images.unsplash.com/photo-1623428187969-5da2dcea5ebf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "210",
+    "category": "Hot-Pot",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 104,
+    "title": "Ice Cream",
+    "image":
+        "https://images.unsplash.com/photo-1623595119708-26b1f7300075?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "180",
+    "category": "Sweet",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 104,
+    "title": "4-Season Salad",
+    "image":
+        "https://images.unsplash.com/photo-1626711934535-9749ea30dba8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "180",
+    "category": "Salad",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 109,
+    "title": "King of Steak",
+    "image":
+        "https://images.unsplash.com/photo-1565299715199-866c917206bb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "350",
+    "category": "Steak",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 103,
+    "title": "Spicy Burger",
+    "image":
+        "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "350",
+    "category": "Fast Food",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 109,
+    "title": "Coconut Cake",
+    "image":
+        "https://images.unsplash.com/photo-1619846227717-205b9dccac17?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "180",
+    "category": "Sweet",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 108,
+    "title": "Original Pizza",
+    "image":
+        "https://images.unsplash.com/photo-1585238342024-78d387f4a707?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "180",
+    "category": "Fast Food",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 107,
+    "title": "Salmon Susi",
+    "image":
+        "https://images.unsplash.com/photo-1633478062482-790e3b5dd810?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "350",
+    "category": "Protein",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 104,
+    "title": "Curry",
+    "image":
+        "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "180",
+    "category": "Soup",
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+  {
+    "id": 105,
+    "title": "Strip Steak",
+    "image":
+        "https://images.unsplash.com/photo-1560781290-7dc94c0f8f4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "view_count": "250",
+    "category": categories[1]["name"],
+    "is_favorited": false,
+    "description":
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+  },
+];
