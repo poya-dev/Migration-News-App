adb5056d7041d32e3e2e51e0b6577fd4b89f08cf
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-04 00:14:54 -0800

    [ADD] Added all part of home screen including sliver appbar, searchbar, category and news items.

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,16 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../widgets/custom_round_textbox.dart';
+import '../widgets/category_item.dart';
+import '../widgets/news_item.dart';
+import '../widgets/icon_box.dart';
+import '../utils/data.dart';
+import '../theme/color.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedCategoryIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text('Home Screen'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            pinned: true,
+            snap: true,
+            elevation: -0,
+            floating: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconBox(
+                      child: SvgPicture.asset(
+                        'assets/icons/profile.svg',
+                        color: darker,
+                        width: 28,
+                        height: 28,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      'Amir mohammad',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                IconBox(
+                  child: SvgPicture.asset(
+                    'assets/icons/logo.svg',
+                    color: darker,
+                    width: 28,
+                    height: 28,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: CustomRoundTextBox(
+                      hint: "Search",
+                      prefix: const Icon(Icons.search, color: Colors.grey),
+                      onTap: () {
+                        // ignore: avoid_print
+                        print('Hello searching...');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.fromLTRB(15, 5, 7, 20),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  categories.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: CategoryItem(
+                      padding: const EdgeInsets.fromLTRB(12, 8, 8, 10),
+                      data: categories[index],
+                      isSelected: index == _selectedCategoryIndex,
+                      onTap: () {
+                        setState(() {
+                          _selectedCategoryIndex = index;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            childCount: news.length,
+            (context, index) => NewsItem(
+              data: news[index],
+            ),
+          ))
+        ],
       ),
     );
   }
