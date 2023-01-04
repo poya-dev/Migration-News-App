44cb0d8293b2c52294a7217805e1c499fdfb5979
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-04 09:31:48 -0800

    [ADD] Added custom appBar widget.

---
+++ lib/src/widgets/custom_app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../widgets/icon_box.dart';
+import '../theme/color.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              IconBox(
+                child: SvgPicture.asset(
+                  'assets/icons/profile.svg',
+                  color: darker,
+                  width: 28,
+                  height: 28,
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text(
+                'Amir mohammad',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          IconBox(
+            child: SvgPicture.asset(
+              'assets/icons/logo.svg',
+              color: darker,
+              width: 28,
+              height: 28,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
