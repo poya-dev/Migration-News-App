ae1b2ee89ec658be01426379c50bdef73b820b7d
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-03 23:06:14 -0800

    [ADD] Added new widget for news item.

---
+++ lib/src/widgets/news_item.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_image.dart';
+import '../theme/color.dart';
+
+class NewsItem extends StatelessWidget {
+  const NewsItem({
+    Key? key,
+    required this.data,
+    this.width = 300,
+    this.onTap,
+    this.onFavoriteTap,
+  }) : super(key: key);
+
+  final Map data;
+  final double width;
+  final GestureTapCallback? onTap;
+  final GestureTapCallback? onFavoriteTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(width: .1, color: darker),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomImage(
+                data["image"],
+                radius: 0,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 250,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 12,
+                ),
+                child: Text(
+                  data['title'],
+                  style: const TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              // ignore: avoid_unnecessary_containers
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.bookmark_outline_outlined,
+                        color: Colors.black38,
+                        size: 32,
+                      ),
+                    ),
+                    Text(
+                      data['category'],
+                      style: const TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.visibility_outlined,
+                            color: Colors.black38,
+                            size: 32,
+                          ),
+                        ),
+                        Text(
+                          data['view_count'],
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
