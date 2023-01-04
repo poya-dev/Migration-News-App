5d4666f35c70c7349aa1ae7f70fb33650bc22608
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-04 09:18:21 -0800

    [IMP] Home screen design improved.

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -21,101 +23,105 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            pinned: true,
-            snap: true,
-            elevation: -0,
-            floating: true,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    IconBox(
-                      child: SvgPicture.asset(
-                        'assets/icons/profile.svg',
-                        color: darker,
-                        width: 28,
-                        height: 28,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    const Text(
-                      'Amir mohammad',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
                 IconBox(
                   child: SvgPicture.asset(
-                    'assets/icons/logo.svg',
+                    'assets/icons/profile.svg',
                     color: darker,
                     width: 28,
                     height: 28,
                   ),
                 ),
+                const SizedBox(
+                  width: 8,
+                ),
+                const Text(
+                  'Amir mohammad',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
               ],
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: CustomRoundTextBox(
-                      hint: "Search",
-                      prefix: const Icon(Icons.search, color: Colors.grey),
-                      onTap: () {
-                        // ignore: avoid_print
-                        print('Hello searching...');
-                      },
-                    ),
-                  ),
-                ],
+            IconBox(
+              child: SvgPicture.asset(
+                'assets/icons/logo.svg',
+                color: darker,
+                width: 28,
+                height: 28,
               ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.fromLTRB(15, 5, 7, 20),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(
-                  categories.length,
-                  (index) => Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: CategoryItem(
-                      padding: const EdgeInsets.fromLTRB(12, 8, 8, 10),
-                      data: categories[index],
-                      isSelected: index == _selectedCategoryIndex,
-                      onTap: () {
-                        setState(() {
-                          _selectedCategoryIndex = index;
-                        });
-                      },
+          ],
+        ),
+      ),
+      body: Container(
+        child: RefreshIndicator(
+          onRefresh: () async {},
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 15, 15),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomRoundTextBox(
+                        hint: "Search",
+                        prefix: const Icon(Icons.search, color: Colors.grey),
+                        onTap: () {
+                          // ignore: avoid_print
+                          print('Hello searching...');
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(15, 5, 7, 20),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    categories.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: CategoryItem(
+                        padding: const EdgeInsets.fromLTRB(12, 8, 8, 10),
+                        data: categories[index],
+                        isSelected: index == _selectedCategoryIndex,
+                        onTap: () {
+                          setState(() {
+                            _selectedCategoryIndex = index;
+                          });
+                        },
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 12,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: news.length,
+                physics: const ScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return NewsItem(
+                    data: news[index],
+                  );
+                },
+              ),
+            ],
           ),
-          SliverList(
-              delegate: SliverChildBuilderDelegate(
-            childCount: news.length,
-            (context, index) => NewsItem(
-              data: news[index],
-            ),
-          ))
-        ],
+        ),
       ),
     );
   }
