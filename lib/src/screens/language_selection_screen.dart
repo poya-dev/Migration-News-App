26efa7edd1c74794e95178d23093071d61a766ee
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-05 04:56:23 -0800

    [ADD] Created new screen for user langauge selection and implemented with initial design.

---
+++ lib/src/screens/language_selection_screen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../widgets/language_item.dart';
+import './root_screen.dart';
+
+class LanguageSelectionScreen extends StatefulWidget {
+  const LanguageSelectionScreen({super.key});
+
+  @override
+  State<LanguageSelectionScreen> createState() =>
+      _LanguageSelectionScreenState();
+}
+
+class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
+  final languages = ['دری', 'پشتو'];
+  int selectedLanguageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+            vertical: 18,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Center(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: SvgPicture.asset(
+                    'assets/icons/logo.svg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 32),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'به',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      'معلومات مهاجرت',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.lightBlue.withOpacity(0.7),
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    const Text(
+                      'خوش آمدید',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 4),
+                child: const Text(
+                  'لطفآ زبان مورد نظر تان را انتخاب کنید',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              ListView(
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                children: List.generate(
+                  languages.length,
+                  (index) => LanguageItem(
+                    icon: Icons.language,
+                    language: languages[index],
+                    isSelected: index == selectedLanguageIndex,
+                    onTap: () {
+                      setState(() {
+                        selectedLanguageIndex = index;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 18),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                        Colors.lightBlue.withOpacity(0.7),
+                      )),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RootScreen(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        'ادامه',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
