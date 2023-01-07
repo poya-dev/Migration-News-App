3835d740991190f13f72380e176653a1ebc8d621
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-07 00:50:59 -0800

    [ADD] Added localization config and locale bloc in app.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import './localization/app_localizations_setup.dart';
+import './blocs/locale/locale_bloc.dart';
+import './blocs/locale/locale_state.dart';
 import './screens/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,10 +11,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'News App',
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LocaleBloc(),
+        ),
+      ],
+      child: BlocBuilder<LocaleBloc, LocaleState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'News App',
+            debugShowCheckedModeBanner: false,
+            locale: state.locale,
+            supportedLocales: const [
+              Locale('en', 'US'),
+              Locale('fa', 'IR'),
+              Locale('ps', 'PT')
+            ],
+            localizationsDelegates: AppLocalizationSetup.localizationsDelegate,
+            localeResolutionCallback: (locale, supportedLocales) {
+              for (var supportedLocale in supportedLocales) {
+                if (supportedLocale.languageCode == locale!.languageCode &&
+                    supportedLocale.countryCode == locale.countryCode) {
+                  return supportedLocale;
+                }
+              }
+            },
+            home: const SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
