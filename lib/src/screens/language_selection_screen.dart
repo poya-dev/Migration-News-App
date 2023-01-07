042d175757c8764b2c2bca3d2b108ee8c5aab567
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-07 06:13:50 -0800

    [ADD] Added Locale bloc for changing locale.

--- lib/src/screens/language_selection_screen.dart
+++ lib/src/screens/language_selection_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../blocs/locale/locale_bloc.dart';
+import '../blocs/locale/locale_event.dart';
+import '../blocs/locale/locale_state.dart';
 import '../widgets/language_item.dart';
+import '../utils/translation_util.dart';
 import './root_screen.dart';
 
 class LanguageSelectionScreen extends StatefulWidget {
@@ -13,133 +18,156 @@ class LanguageSelectionScreen extends StatefulWidget {
 }
 
 class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
-  final languages = ['دری', 'پشتو'];
+  final languages = ['دری', 'پشتو', 'English'];
   int selectedLanguageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 32,
-          vertical: 18,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Center(
-              child: SizedBox(
-                height: 200,
-                width: 200,
-                child: SvgPicture.asset(
-                  'assets/icons/logo.svg',
-                  fit: BoxFit.cover,
-                ),
-              ),
+      body: BlocBuilder<LocaleBloc, LocaleState>(
+        builder: (context, state) {
+          return Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 18,
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 32),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Text(
-                    'به',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w900,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Center(
+                  child: SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: SvgPicture.asset(
+                      'assets/icons/logo.svg',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    'معلومات مهاجرت',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.lightBlue,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    'خوش آمدید',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 4),
-              child: const Text(
-                'لطفآ زبان مورد نظر تان را انتخاب کنید',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w900,
                 ),
-              ),
-            ),
-            ListView(
-              physics: const ScrollPhysics(),
-              shrinkWrap: true,
-              children: List.generate(
-                languages.length,
-                (index) => LanguageItem(
-                  icon: Icons.language,
-                  language: languages[index],
-                  isSelected: index == selectedLanguageIndex,
-                  onTap: () {
-                    setState(() {
-                      selectedLanguageIndex = index;
-                    });
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                      Colors.lightBlue,
-                    )),
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RootScreen(),
+                Container(
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'به',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w900,
                         ),
-                      );
-                    },
-                    child: const Text(
-                      'ادامه',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
                       ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        'معلومات مهاجرت',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.lightBlue,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        'خوش آمدید',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    getTranslated(context, 'lang_selection_instruction'),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
-                ],
-              ),
+                ),
+                ListView(
+                  physics: const ScrollPhysics(),
+                  shrinkWrap: true,
+                  children: List.generate(
+                    languages.length,
+                    (index) => LanguageItem(
+                      icon: Icons.language,
+                      language: languages[index],
+                      isSelected: index == selectedLanguageIndex,
+                      onTap: () {
+                        setState(
+                          () {
+                            selectedLanguageIndex = index;
+                            changeLanguage(context, index);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                          Colors.lightBlue,
+                        )),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const RootScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'ادامه',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
+  }
+
+  void changeLanguage(BuildContext context, int index) {
+    if (index == 0) {
+      context
+          .read<LocaleBloc>()
+          .add(const LocaleChanged(lang: Language.persian));
+    } else if (index == 1) {
+      context
+          .read<LocaleBloc>()
+          .add(const LocaleChanged(lang: Language.pashto));
+    } else {
+      context
+          .read<LocaleBloc>()
+          .add(const LocaleChanged(lang: Language.english));
+    }
   }
 }
