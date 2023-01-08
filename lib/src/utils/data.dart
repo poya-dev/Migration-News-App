3f2fc6817edeb5996519f2bd7da7f88235f79d53
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-07 22:58:20 -0800

    [IMP] Dummy data changed to persian.

--- lib/src/utils/data.dart
+++ lib/src/utils/data.dart
@@ -1,144 +1,144 @@
 List categories = [
-  {"name": "All"},
-  {"name": "Sport"},
-  {"name": "Health"},
-  {"name": "Culture"},
-  {"name": "Technology"},
-  {"name": "People"},
-  {"name": "Food"},
+  {"name": "همه"},
+  {"name": "خطرات"},
+  {"name": "مهاجرت"},
+  {"name": "فرصتها"},
+  {"name": "تنکالوژی"},
+  {"name": "ورزش"},
+  {"name": "غذا"},
 ];
 
 List news = [
   {
     "id": 102,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "320",
-    "category": "Salad",
+    "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 100,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1623428187969-5da2dcea5ebf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "210",
-    "category": "Hot-Pot",
+    "category": categories[2]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 104,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1623595119708-26b1f7300075?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "180",
     "category": "Sweet",
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 104,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1626711934535-9749ea30dba8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "180",
-    "category": "Salad",
+    "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 109,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1565299715199-866c917206bb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "350",
-    "category": "Steak",
+    "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 103,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "350",
-    "category": "Fast Food",
+    "category": categories[3]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 109,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1619846227717-205b9dccac17?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "180",
-    "category": "Sweet",
+    "category": categories[4]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 108,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1585238342024-78d387f4a707?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "180",
-    "category": "Fast Food",
+    "category": categories[2]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 107,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1633478062482-790e3b5dd810?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "350",
-    "category": "Protein",
+    "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 104,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "180",
-    "category": "Soup",
+    "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
   {
     "id": 105,
     "title":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
     "image":
         "https://images.unsplash.com/photo-1560781290-7dc94c0f8f4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGZhc2hpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "view_count": "250",
     "category": categories[1]["name"],
     "is_favorited": false,
     "description":
-        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+        "نجات 43 تن از مهاجر از آب های بحیره اژه توسط گارد های ساحلی ترکیه",
   },
 ];
