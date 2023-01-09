10afbad71cb01d9264780be69ae04f18ae5dd6cd
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-09 02:16:40 -0800

    [ADD] Created custom textFormField in widget dir.

---
+++ lib/src/widgets/custom_form_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({
+    Key? key,
+    required this.hintText,
+    this.keyboardType,
+    this.validator,
+  }) : super(key: key);
+
+  final String hintText;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: keyboardType,
+        validator: validator,
+        decoration: InputDecoration(hintText: hintText),
+      ),
+    );
+  }
+}
