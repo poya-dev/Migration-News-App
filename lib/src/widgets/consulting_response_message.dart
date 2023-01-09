531247a5cfef4372db1dbca1cd858ea73d670dcb
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-09 00:04:46 -0800

    [ADD] Added new widget for phone consulting response message.

---
+++ lib/src/widgets/consulting_response_message.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ConsultingResponseMessage extends StatelessWidget {
+  const ConsultingResponseMessage({
+    Key? key,
+    required this.timeAgo,
+    required this.message,
+  }) : super(key: key);
+
+  final String timeAgo;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 6,
+        vertical: 8,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black.withOpacity(0.05),
+          style: BorderStyle.solid,
+          width: 1.5,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            blurStyle: BlurStyle.outer,
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10.0,
+            spreadRadius: .5,
+          ),
+        ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 6,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Text(
+                    timeAgo,
+                    style: const TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4),
+              child: Text(
+                message,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
