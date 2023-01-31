104c8924dace8e84dc288aac95703211793a11a1
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-30 21:58:23 -0800

    [ADD] Added client api service and two static methods for googlel and facebook sign in to comminicate with nodejs api endpoints

---
+++ lib/src/api/api.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/user.dart';
+
+const baseUrl = 'http://192.168.13.96:3000/api/v1/';
+
+class ApiService {
+  static final http.Client client = http.Client();
+  static Future<User> signInWithGoogle(String idToken) async {
+    try {
+      final response = await client.get(Uri.parse('$baseUrl/auth/google'),
+          headers: {
+            'Authorization': 'Bearer $idToken',
+            'Content-Type': 'application/json'
+          });
+      final resJson = jsonDecode(response.body);
+      return User.fromJson(resJson);
+    } catch (e) {
+      throw Exception('---inside api Something went wrong ---> $e');
+    }
+  }
+
+  static Future<User> signInWithFacebook(String accessToken) async {
+    try {
+      final response = await client.post(Uri.parse('$baseUrl/auth/facebook'),
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json'
+          });
+      final resJson = jsonDecode(response.body);
+      return User.fromJson(resJson);
+    } catch (e) {
+      throw Exception('Something went wrong ---> $e');
+    }
+  }
+}
