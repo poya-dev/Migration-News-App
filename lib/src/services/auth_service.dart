063b36ede711debfdb459dcb16604ba48c168071
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-31 00:33:22 -0800

    [ADD] Added facebookSignIn service in authService and return token or exception

--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthService {
   static Future<String> googleSignIn() async {
@@ -13,5 +14,18 @@ class AuthService {
       throw Exception('Sign up with Google is failed');
     }
     throw Exception('Sign up with Google is failed');
+  }
+
+  static Future<String> facebookSignIn() async {
+    const scope = ['public_profile', 'email'];
+    final loginResult = await FacebookAuth.instance.login(permissions: scope);
+    if (loginResult.status == LoginStatus.success) {
+      final AccessToken accessToken = loginResult.accessToken!;
+      return accessToken.token;
+    }
+    if (loginResult.status == LoginStatus.cancelled) {
+      throw Exception('User cancelled facebook Sign In');
+    }
+    throw Exception('Something went wrong with facebook Sign In');
   }
 }
