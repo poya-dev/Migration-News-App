c23eb5d0f0a87b0cb253742be82d6dc913ac7033
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-31 00:35:38 -0800

    [ADD] Added eventHandler for facebookSignInRequested event which retuns either user or error from web API

--- lib/src/blocs/auth/auth_bloc.dart
+++ lib/src/blocs/auth/auth_bloc.dart
@@ -2,14 +2,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../repositories/user_repository.dart';
 import '../../services/auth_service.dart';
-import 'auth_event.dart';
-import 'auth_state.dart';
+import './auth_event.dart';
+import './auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserRepository userRepository;
   AuthBloc({required this.userRepository}) : super(UnAuthenticated()) {
     on<GoogleSignInRequested>(
-      ((event, emit) async {
+      (AuthEvent event, Emitter<AuthState> emit) async {
         emit(AuthLoading());
         try {
           final idToken = await AuthService.googleSignIn();
@@ -19,7 +19,20 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           emit(AuthError(error: e.toString()));
           emit(UnAuthenticated());
         }
-      }),
+      },
+    );
+    on<FacebookSignInRequested>(
+      (AuthEvent event, Emitter<AuthState> emit) async {
+        emit(AuthLoading());
+        try {
+          final accessToken = await AuthService.facebookSignIn();
+          final user = await userRepository.signInWithFacebook(accessToken);
+          emit(Authenticated(user: user));
+        } catch (e) {
+          emit(AuthError(error: e.toString()));
+          emit(UnAuthenticated());
+        }
+      },
     );
   }
 }
