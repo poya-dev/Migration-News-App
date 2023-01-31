4d7fcfa9ca910725fb24cfec5009d73842505cdb
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-31 09:53:29 -0800

    [ADD] Added method for fetching news catogory from api

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import '../models/user.dart';
+import '../models/category.dart';
 
 const baseUrl = 'http://192.168.13.96:3000/api/v1/';
 
@@ -12,29 +13,52 @@ class ApiService {
   static final http.Client client = http.Client();
   static Future<User> signInWithGoogle(String idToken) async {
     try {
-      final response = await client.get(Uri.parse('$baseUrl/auth/google'),
-          headers: {
-            'Authorization': 'Bearer $idToken',
-            'Content-Type': 'application/json'
-          });
+      final response = await client.get(
+        Uri.parse('$baseUrl/auth/google'),
+        headers: {
+          'Authorization': 'Bearer $idToken',
+          'Content-Type': 'application/json'
+        },
+      );
       final resJson = jsonDecode(response.body);
       return User.fromJson(resJson);
-    } catch (e) {
-      throw Exception('---inside api Something went wrong ---> $e');
+    } catch (error) {
+      throw Exception(error.toString());
     }
   }
 
   static Future<User> signInWithFacebook(String accessToken) async {
     try {
-      final response = await client.post(Uri.parse('$baseUrl/auth/facebook'),
-          headers: {
-            'Authorization': 'Bearer $accessToken',
-            'Content-Type': 'application/json'
-          });
+      final response = await client.post(
+        Uri.parse('$baseUrl/auth/facebook'),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json'
+        },
+      );
       final resJson = jsonDecode(response.body);
       return User.fromJson(resJson);
-    } catch (e) {
-      throw Exception('Something went wrong ---> $e');
+    } catch (error) {
+      throw Exception(error.toString());
+    }
+  }
+
+  static Future<List<Category>> getCategories(String accessToken) async {
+    try {
+      final response = await client.get(
+        Uri.parse('$baseUrl/client/news-category?langCode=us'),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json'
+        },
+      );
+      final resJson = jsonDecode(response.body);
+      final List<Category> categoryList = resJson['data']
+          .map<Category>((category) => Category.fromJson(category))
+          .toList();
+      return categoryList;
+    } catch (error) {
+      throw Exception(error.toString());
     }
   }
 }
