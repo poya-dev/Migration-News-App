c265e5e1ce89d739fd02c149a63d880c4f1f858b
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 08:48:53 -0800

    [IMP] Added debouncing which mitigates api call

--- lib/src/blocs/news/news_bloc.dart
+++ lib/src/blocs/news/news_bloc.dart
@@ -1,16 +1,27 @@
-import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stream_transform/stream_transform.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 
 import '../../repositories/news_repository.dart';
 import './news_event.dart';
 import './news_state.dart';
 
 var page = 1;
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
 
 class NewsBloc extends Bloc<NewsEvent, NewsState> {
   final NewsRepository newsRepository;
   NewsBloc({required this.newsRepository}) : super(const NewsState()) {
-    on<NewsFetched>(_onNewsFetched);
+    on<NewsFetched>(
+      _onNewsFetched,
+      transformer: throttleDroppable(throttleDuration),
+    );
   }
 
   Future _onNewsFetched(NewsFetched event, Emitter<NewsState> emit) async {
@@ -20,22 +31,25 @@ class NewsBloc extends Bloc<NewsEvent, NewsState> {
       if (state.hasReachedMax) return;
       if (state.status == Status.initial) {
         final response = await newsRepository.getNews(token);
-        final hasReachedMax = response.currentPage == response.lastPage;
+        bool reachedMax =
+            response.currentPage! >= response.lastPage! ? true : false;
+        page++;
         return emit(
           state.copyWith(
             news: response.data,
             status: Status.success,
-            hasReachedMax: hasReachedMax,
+            hasReachedMax: reachedMax,
           ),
         );
       }
       final response = await newsRepository.getNews(token, page);
-      final hasReachedMax = response.currentPage == response.lastPage;
+      bool reachedMax =
+          response.currentPage! >= response.lastPage! ? true : false;
       emit(
         state.copyWith(
-          news: response.data,
+          news: List.of(state.news)..addAll(response.data!),
           status: Status.success,
-          hasReachedMax: hasReachedMax,
+          hasReachedMax: reachedMax,
         ),
       );
       page++;
