2823d44d1e949e6f434fe71ff0cee2bd1b0f0fbd
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 04:09:02 -0800

    [ADD] Added method for fetching news also used response generic class and some improvemnts

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -6,6 +6,8 @@ import 'package:http/http.dart' as http;
 
 import '../models/user.dart';
 import '../models/category.dart';
+import '../models/news.dart';
+import './response.dart';
 
 const baseUrl = 'http://192.168.13.96:3000/api/v1/';
 
@@ -20,43 +22,69 @@ class ApiService {
           'Content-Type': 'application/json'
         },
       );
-      final resJson = jsonDecode(response.body);
+      final resJson = json.decode(response.body);
       return User.fromJson(resJson);
     } catch (error) {
       throw Exception(error.toString());
     }
   }
 
-  static Future<User> signInWithFacebook(String accessToken) async {
+  static Future<User> signInWithFacebook(String token) async {
     try {
       final response = await client.post(
         Uri.parse('$baseUrl/auth/facebook'),
         headers: {
-          'Authorization': 'Bearer $accessToken',
+          'Authorization': 'Bearer $token',
           'Content-Type': 'application/json'
         },
       );
-      final resJson = jsonDecode(response.body);
+      final resJson = json.decode(response.body);
       return User.fromJson(resJson);
     } catch (error) {
       throw Exception(error.toString());
     }
   }
 
-  static Future<List<Category>> getCategories(String accessToken) async {
+  static Future<Response<List<Category>>> getCategories(String token) async {
     try {
       final response = await client.get(
-        Uri.parse('$baseUrl/client/news-category?langCode=us'),
-        headers: {
-          'Authorization': 'Bearer $accessToken',
-          'Content-Type': 'application/json'
-        },
+        Uri.parse('$baseUrl/client/news-category'),
+        headers: {'Authorization': 'Bearer $token'},
       );
-      final resJson = jsonDecode(response.body);
-      final List<Category> categoryList = resJson['data']
-          .map<Category>((category) => Category.fromJson(category))
-          .toList();
-      return categoryList;
+      final body = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final List<Category> categoryList =
+            body['data'].map<Category>((categoryItem) {
+          return Category.fromJson(categoryItem);
+        }).toList();
+        return Response(data: categoryList);
+      }
+      throw Exception('Something went wrong ---> ${response.body}');
+    } catch (error) {
+      throw Exception(error.toString());
+    }
+  }
+
+  static Future<Response<List<News>>> getNews(String token, int page) async {
+    try {
+      final response = await client.get(
+        Uri.parse('$baseUrl/client/news'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+        final currentPage = body['currentPage'];
+        final lastPage = body['lastPage'];
+        final List<News> newsList = body['data'].map<News>((newsItem) {
+          return News.fromJson(newsItem);
+        }).toList();
+        return Response(
+          data: newsList,
+          currentPage: currentPage,
+          lastPage: lastPage,
+        );
+      }
+      throw Exception('Something went wrong ---> ${response.body}');
     } catch (error) {
       throw Exception(error.toString());
     }
