dbc342bb28822c01b0bbec2b16bb93efa21704df
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 04:14:20 -0800

    [ADD] Added event for news

---
+++ lib/src/blocs/news/news_event.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+abstract class NewsEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class NewsFetched extends NewsEvent {
+  NewsFetched(this.accessToken);
+
+  final String accessToken;
+
+  @override
+  List<Object> get props => [accessToken];
+}
