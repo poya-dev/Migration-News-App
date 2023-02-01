2613f70d24b5edd4fdb5a212278ed4634583e31f
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 09:13:01 -0800

    [ADD] Added constant file and defined all API endpoints

---
+++ lib/src/utils/constant.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  static String root = 'http://192.168.13.96:3000';
+  static String baseUrl = '$root/api/v1/';
+  static String googleSinIn = '$baseUrl/auth/google';
+  static String facebookSinIn = '$baseUrl/auth/facebook';
+  static String category = '$baseUrl/client/news-category';
+  static String news = '$baseUrl/client/news';
+}
