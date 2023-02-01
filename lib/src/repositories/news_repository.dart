37cc80dbe14d5ffa3655ddee3a587cb282be0eec
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 04:10:21 -0800

    [ADD] Added method for getting all news, in news repository

--- lib/src/repositories/news_repository.dart
+++ lib/src/repositories/news_repository.dart
@@ -1,8 +1,14 @@
 import '../models/category.dart';
+import '../models/news.dart';
+import '../api/response.dart';
 import '../api/api.dart';
 
 class NewsRepository {
-  Future<List<Category>> getCategories(String idToken) async {
+  Future<Response<List<Category>>> getCategories(String idToken) async {
     return await ApiService.getCategories(idToken);
+  }
+
+  Future<Response<List<News>>> getNews(String idToken, [int page = 1]) async {
+    return await ApiService.getNews(idToken, page);
   }
 }
