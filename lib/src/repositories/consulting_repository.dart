e1fdf6687af698c3a3c91e7965602a44d8fca8d8
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-04 06:54:05 -0800

    [ADD] Added method in repository which calls the api method

--- lib/src/repositories/consulting_repository.dart
+++ lib/src/repositories/consulting_repository.dart
@@ -1,9 +1,19 @@
 import '../models/consulting_request.dart';
+import '../models/consulting.dart';
+import '../api/response.dart';
 import '../api/api.dart';
 
 class ConsultingRepository {
   Future<void> consultingRequest(
-      String accessToken, ConsultingRequest data) async {
+    String accessToken,
+    ConsultingRequest data,
+  ) async {
     return await ApiService.consultingRequest(accessToken, data);
+  }
+
+  Future<Response<List<ConsultingResponse>>> getConsultingResponse(
+    String accessToken,
+  ) async {
+    return await ApiService.getConsultingResponse(accessToken);
   }
 }
