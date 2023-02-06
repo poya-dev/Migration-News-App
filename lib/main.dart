545ed8bb2e364be4e13ff1454ce35e8ddcf3ec05
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-06 05:02:37 -0800

    [ADD] Firebase initialized, also FCM background message handler implemeneted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import './src/app.dart';
 
-void main() => runApp(const MyApp());
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message by FCM ---> ${message.messageId}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(const MyApp());
+}
