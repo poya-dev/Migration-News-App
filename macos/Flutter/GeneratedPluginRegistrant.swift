882e95e4ef09281791ce86b7222f53c6fec3f84c
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-06 04:59:27 -0800

    [ADD] Added configurations by firebase_cli and flutterfire for FCM usage

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import facebook_auth_desktop
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import flutter_secure_storage_macos
 import path_provider_macos
 import shared_preferences_macos
@@ -14,6 +17,9 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
