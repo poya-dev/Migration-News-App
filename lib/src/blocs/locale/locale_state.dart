079b9b54f53e2b38f56e9c14857e0ff22a798670
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-11 02:33:16 -0800

    [IMP] Locale state improved

--- lib/src/blocs/locale/locale_state.dart
+++ lib/src/blocs/locale/locale_state.dart
@@ -3,14 +3,19 @@ import 'package:equatable/equatable.dart';
 
 import './locale_event.dart' show Language;
 
-class LocaleState extends Equatable {
-  const LocaleState({
-    required this.locale,
-    required this.lang,
-  });
-
+class AppLocaleState extends Equatable {
   final Locale locale;
   final Language lang;
+
+  AppLocaleState({
+    this.locale = const Locale('fa'),
+    this.lang = Language.persian,
+  });
+
+  copyWith({Locale? locale, Language? lang}) => AppLocaleState(
+        locale: locale ?? this.locale,
+        lang: lang ?? this.lang,
+      );
 
   @override
   List<Object> get props => [locale, lang];
