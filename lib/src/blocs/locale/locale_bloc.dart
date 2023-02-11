da80b8b27e143c20a0a562cb6cb1bcf4524f2359
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-11 02:34:53 -0800

    [ADD] Considered persisting user selected langauge in locale bloc

--- lib/src/blocs/locale/locale_bloc.dart
+++ lib/src/blocs/locale/locale_bloc.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../preferences/language_prefs.dart';
 import './locale_event.dart';
 import './locale_state.dart';
 
-class LocaleBloc extends Bloc<LocaleEvent, LocaleState> {
-  LocaleBloc()
-      : super(
-          const LocaleState(locale: Locale('fa'), lang: Language.english),
-        ) {
+class LocaleBloc extends Bloc<LocaleEvent, AppLocaleState> {
+  LocaleBloc() : super(AppLocaleState()) {
     on<LocaleChanged>(
-      (LocaleChanged event, Emitter<LocaleState> emit) => {
-        if (event.lang == Language.english)
-          emit(const LocaleState(locale: Locale('en'), lang: Language.english))
-        else if (event.lang == Language.persian)
-          emit(const LocaleState(locale: Locale('fa'), lang: Language.persian))
-        else
-          emit(const LocaleState(locale: Locale('ps'), lang: Language.pashto))
+      (LocaleChanged event, Emitter<AppLocaleState> emit) {
+        if (event.lang == Language.persian) {
+          LanguagePrefs.setLocale(event.lang);
+          return emit(state.copyWith(
+            lang: Language.persian,
+            locale: Locale('fa'),
+          ));
+        } else {
+          LanguagePrefs.setLocale(event.lang);
+          return emit(state.copyWith(
+            lang: Language.pashto,
+            locale: Locale('ps'),
+          ));
+        }
       },
     );
   }
