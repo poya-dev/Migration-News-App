0143963b296cc17a7a5a5f7b6bb5d4469548597b
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-11 02:32:13 -0800

    [ADD] Added language pref for persisting user selected language

---
+++ lib/src/preferences/language_prefs.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import '../blocs/locale/locale_event.dart' show Language;
+import './preferences.dart';
+
+class LanguagePrefs {
+  static void setLocale(Language? language) {
+    if (language == null) {
+      language = Language.persian;
+    }
+    String lang = jsonEncode(language.toString());
+    Preferences.preferences!.setString(
+      Preferences.LANGUAGE_CODE,
+      lang,
+    );
+  }
+
+  static Language? getLocale() {
+    String? language =
+        Preferences.preferences!.getString(Preferences.LANGUAGE_CODE);
+    if (language == null) {
+      return null;
+    }
+    return getLangFromString(jsonDecode(language));
+  }
+
+  static Language? getLangFromString(String lang) {
+    for (Language language in Language.values) {
+      if (language.toString() == lang) {
+        return language;
+      }
+    }
+    return null;
+  }
+}
