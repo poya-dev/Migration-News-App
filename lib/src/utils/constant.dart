edb1cfab370883045e74b6f3d501e511369074e9
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-14 01:53:09 -0800

    Server IP Address changed

--- lib/src/utils/constant.dart
+++ lib/src/utils/constant.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static String root = 'http://192.168.13.96:3000';
+  static String root = 'http://192.168.117.96:3000';
   static String baseUrl = '$root/api/v1/';
   static String googleSinIn = '$baseUrl/auth/google';
   static String facebookSinIn = '$baseUrl/auth/facebook';
