1d6ac64874261e0cd46d6d88286243d6386fde06
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-15 03:22:23 -0800

    Removed unnecessary import

--- lib/src/services/fcm_service.dart
+++ lib/src/services/fcm_service.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
