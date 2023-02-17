d9183d0bf845fa071bbcd8a1f3aaad2fab9550ad
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-17 07:24:25 -0800

    [ADD] Added widget for displaying error

---
+++ lib/src/widgets/make_error.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MakeError extends StatelessWidget {
+  const MakeError({
+    super.key,
+    required this.error,
+    required this.onTap,
+  });
+
+  final String error;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.error_outline,
+            size: 50,
+            color: Colors.black45,
+          ),
+          const SizedBox(height: 12),
+          Text(
+            error,
+            style: TextStyle(
+              fontSize: 18,
+              fontFamily: 'BNazann',
+            ),
+          ),
+          SizedBox(height: 18),
+          ElevatedButton(
+            onPressed: onTap,
+            child: Text('تلاش مجدد'),
+          )
+        ],
+      ),
+    );
+  }
+}
